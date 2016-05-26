95462375d345a85e5a8af01109821718406ff0e0
Luis Nabais <dextro@gmail.com>
2016-05-26 11:59:41 +0100

    Count number of discarded lines

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ var Utils = require('./utils'),
     Writer = require("./writers/csv"),
     Reader = require("./readers/ann");
 
-function filterLines(entry) {
+const TAG = "CORE";
+
+function filterLine(entry) {
     return entry["year"] && (entry["Genres"].length > 0 || entry["Themes"] > 0);
+}
+
+function filterLines(data) {
+    var count = data.length;
+
+    data = data.filter(filterLine);
+
+    count = count - data.length;
+
+    if (count > 0) {
+        console.log(`[${TAG}] Discarded ${count} lines`);
+    }
+
+    return data;
 }
 
 function processPage(idx) {
@@ -12,8 +28,9 @@ function processPage(idx) {
     return Reader.getPage(idx)
         .then(data => {
             continueProcess = data.length == Reader.PAGE_SIZE;
-            return data.filter(filterLines);
+            return data;
         })
+        .then(filterLines)
         .then(Writer.save)
         .then(Utils.delay())
         .then(() => continueProcess && processPage(idx + 1));
