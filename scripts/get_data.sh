a8b22e641ac435db976d6df40959b1fa8017f350
Luis Nabais <dextro@gmail.com>
2018-02-25 21:46:36 +0100

    Fix missing genres.json export on get_data.sh script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -12,10 +12,11 @@ curl "http://localhost:5984/ann/_design/aggregated/_view/byKey?group=true" > tar
 
 curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByKeyAndType?group=true" > target/data/episode_data.json
 
-curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > target/data/genre.json
+curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > target/data/genres.json
 curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > target/data/themes.json
 
 curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > target/data/data_genre.json
 curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > target/data/data_themes.json
 
+curl "http://localhost:5984/ann/_design/genres/_view/all?group=true" > target/data/genres.json
 curl "http://localhost:5984/ann/_design/types/_view/byYear?group=true" > target/data/types.json
