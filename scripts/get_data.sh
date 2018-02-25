a85f4405ae38cf610ade432c989922b6dd5459f9
Luis Nabais <dextro@gmail.com>
2018-02-25 21:44:58 +0100

    Fix get_data script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-curl "http://localhost:5984/ann/_design/aggregated/_view/all?group=true" > web/data/totals.json
+if [ ! -d "target/data" ]; then
+    mkdir target/data;
+fi
 
-curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByType?group=true" > web/data/episode_totals.json
+curl "http://localhost:5984/ann/_design/aggregated/_view/all?group=true" > target/data/totals.json
 
-curl "http://localhost:5984/ann/_design/aggregated/_view/byKey?group=true" > web/data/data.json
+curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByType?group=true" > target/data/episode_totals.json
 
-curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByKeyAndType?group=true" > web/data/episode_data.json
+curl "http://localhost:5984/ann/_design/aggregated/_view/byKey?group=true" > target/data/data.json
 
-curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > web/data/genre.json
-curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > web/data/themes.json
+curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByKeyAndType?group=true" > target/data/episode_data.json
 
-curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > web/data/data_genre.json
-curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > web/data/data_themes.json
+curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > target/data/genre.json
+curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > target/data/themes.json
 
-curl "http://localhost:5984/ann/_design/types/_view/byYear?group=true" > web/data/types.json
+curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > target/data/data_genre.json
+curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > target/data/data_themes.json
+
+curl "http://localhost:5984/ann/_design/types/_view/byYear?group=true" > target/data/types.json
