01041e15d40685c7ccbedafc04d8f2243b879fc4
Luis Nabais <dextro@gmail.com>
2018-02-25 22:03:46 +0100

    Fix data_genres.json filename on get_data.sh script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -15,7 +15,7 @@ curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByKeyAndType?gr
 curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > target/data/genres.json
 curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > target/data/themes.json
 
-curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > target/data/data_genre.json
+curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > target/data/data_genres.json
 curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > target/data/data_themes.json
 
 curl "http://localhost:5984/ann/_design/genres/_view/all?group=true" > target/data/genres.json
