eafe5cdc2162d5d777be0f2c7ba333a0cce5cd78
Luis Nabais <dextro@gmail.com>
2020-02-07 15:39:42 +0000

    Tweak get_data.sh script

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -12,11 +12,11 @@ curl "http://localhost:5984/ann/_design/aggregated/_view/byKey?group=true" > dat
 
 curl "http://localhost:5984/ann/_design/aggregated/_view/episodesByKeyAndType?group=true" > data/episode_data.json
 
-curl "http://localhost:5984/ann/_design/genre/_view/all?group=true" > data/genres.json
-curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > data/themes.json
-
-curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > data/data_genres.json
-curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > data/data_themes.json
 
 curl "http://localhost:5984/ann/_design/genres/_view/all?group=true" > data/genres.json
+curl "http://localhost:5984/ann/_design/genres/_view/byYear?group=true" > data/data_genres.json
+
+curl "http://localhost:5984/ann/_design/themes/_view/all?group=true" > data/themes.json
+curl "http://localhost:5984/ann/_design/themes/_view/byYear?group=true" > data/data_themes.json
+
 curl "http://localhost:5984/ann/_design/types/_view/byYear?group=true" > data/types.json
