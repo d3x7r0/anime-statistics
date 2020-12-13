db6aab7ebe79cba0fd8eb02979255697bc203b21
Luis Nabais <dextro@gmail.com>
2020-12-13 11:51:59 +0000

    Bump dependency versions

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,24 +1,25 @@
 /* eslint-env node */
 /* eslint-disable import/order */
 const { resolve } = require('path')
+const withPreact = require('next-plugin-preact')
 
 const TRANSPILED_MODULES = [
   '@nonsensebb/components',
+  'lodash-es',
 ]
 
 const withTM = require('next-transpile-modules')(TRANSPILED_MODULES)
 
 const nextConfig = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_URL || '',
-  exportTrailingSlash: true,
+  trailingSlash: true,
   experimental: {
     modern: true,
-    polyfillsOptimization: true,
   },
   cssLoaderOptions: {
     url: false,
   },
-  webpack(config, { dev, isServer }) {
+  webpack(config) {
     TRANSPILED_MODULES.forEach((moduleName) => {
       const pkg = require(resolve(__dirname, '.', 'node_modules', moduleName, 'package.json'))
 
@@ -34,40 +35,14 @@ const nextConfig = {
       },
     )
 
-    const splitChunks = config.optimization && config.optimization.splitChunks
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules,
-        })
-        cacheGroups.commons.name = 'framework'
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules,
-        }
-      }
-    }
-
     // Install webpack aliases:
     const aliases = config.resolve.alias || (config.resolve.alias = {})
-    aliases.react = aliases['react-dom'] = 'preact/compat'
-    aliases.preact = resolve(__dirname, 'node_modules', "preact")
-
-    // inject Preact DevTools
-    if (dev && !isServer) {
-      const entry = config.entry
-      config.entry = () => entry().then(entries => {
-        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || [])
-        return entries
-      })
-    }
+    aliases.lodash = 'lodash-es'
 
     return config
   },
 }
 
-module.exports = withTM(nextConfig)
+module.exports = withTM(
+  withPreact(nextConfig),
+)
